b8fb8b215cdda13482b8d3d88f6b5262f25a769b
Faerbit <faerbit@gmail.com>
2017-07-11 08:03:53 +0200

    Added rinkeby to truffle.js

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8700,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+        host: "localhost",
+        port: 8546,
+        network_id: "4" // match rinkeby
     }
   }
 };
